a481055c131a07bbad43a392fdf2ab7c4718f229
Kasumi.S <kasumi@Kasumis-MacBook-Air.local>
2018-12-14 19:34:21 +0100

    update

--- header.php
+++ header.php
@@ -5,6 +5,10 @@
 	<meta name="description" content="Google等の検索結果に表示させるブログの概要" />
 	<title>ケバブログ</title>
 	<link rel="stylesheet" type="text/css" href="style.css" media="all" />
+	<!-- viewport 読み込み -->
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<!-- bootstrap css-->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">"
 	<!-- google fonts -->
 	<link href="https://fonts.googleapis.com/earlyaccess/mplus1p.css" rel="stylesheet" />
 	<link href="https://fonts.googleapis.com/css?family=Titillium+Web" rel="stylesheet">
@@ -12,6 +16,10 @@
 	<link rel="shortcut icon" href="images/favicon.ico"/>
 </head>
 <body>
+<div class="row">
+<div class="col-12">
 	<div id="header">
 		<h1>ケバブログ</h1>
-    </div>
+	</div>
+	</div>
+</div>
